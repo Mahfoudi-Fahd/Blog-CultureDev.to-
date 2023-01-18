fac465d13be70ed9f94aa1fcec9382ba865f19fa
FAHD <mahfoudifahd55@gmail.com>
2023-01-18 17:28:06 +0100

    BCT-2 side bar added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
-const toggleBtn = document.querySelector('.sidebar-toggle');
-const sidebar = document.querySelector('.sidebar');
-
-toggleBtn.addEventListener('click', function() {
-  sidebar.classList.toggle('open');
-});
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
--- pages/dashboard.php
+++ pages/dashboard.php
@@ -22,30 +22,38 @@ else{
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Welcome <?php echo $user["username"]; ?></h1>
-    <a href="logout.php">Logout</a>
+    <!-- <h1>Welcome <?php echo $user["username"]; ?></h1>
+    <a href="logout.php">Logout</a> -->
     
     <!-- sidebar -->
-    <div class="sidebar">
-  <a href="#" class="sidebar-toggle">Toggle Sidebar</a>
-  <nav>
-    <a href="#">Home</a>
-    <a href="#">About</a>
-    <a href="#">Contact</a>
-  </nav>
+    <div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="#">About</a>
+  <a href="#">Services</a>
+  <a href="#">Clients</a>
+  <a href="#">Contact</a>
+</div>
+
+<!-- Use any element to open the sidenav -->
+<span onclick="openNav()">open</span>
+
+<!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page -->
+<div id="main">
+<h1>Welcome <?php echo $user["username"]; ?></h1>
+    <a href="logout.php">Logout</a>
 </div>
 
 </div>
 
 <!-- cards -->
 
-    <div class="card" style="width:18rem;">
+    <!-- <div class="card" style="width:18rem;">
         <div class="card-body">
             <h5 class="card-title">nombre d'articles</h5>
 
         </div>
 
-    </div>
+    </div> -->
   
 
   </body>
