6aa9a11757a09c79e52b44321e5c26fcc5c8b8ca
FAHD <mahfoudifahd55@gmail.com>
2023-01-19 14:36:05 +0100

    BCT-2 split side bar and iclud it in the created posts and categories pages

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,15 +1,15 @@
 <?php
-require '../controllers/functions.php';
+// require '../controllers/functions.php';
 
-$select = new Select();
+// $select = new Select();
 
-if(isset($_SESSION["id"])){
-  $user = $select->selectUserById($_SESSION["id"]);
+// if(isset($_SESSION["id"])){
+//   $user = $select->selectUserById($_SESSION["id"]);
   
-}
-else{
-  header("Location: signin.php");
-}
+// }
+// else{
+//   header("Location: signin.php");
+// }
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -17,10 +17,7 @@ else{
     <meta charset="UTF-8">
     <title> dashboard </title>
     <link rel="stylesheet" href="../assets/style.css">
-    
-    <!-- Boxicons CDN Link -->
-    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
    </head>
 
 
@@ -28,59 +25,9 @@ else{
   <body>
     
 <!-- sidebar -->
-    <div class="sidebar">
-    <div class="logo-details">
-      <i class='bx bxl-c-plus-plus icon'></i>
-        <div class="logo_name">CultureDev</div>
-        <i class='bx bx-menu' id="btn" ></i>
-    </div>
-    <ul class="nav-list">
-    
-      <li>
-        <a href="dashboard.php">
-          <i class='bx bx-grid-alt'></i>
-          <span class="links_name">Dashboard</span>
-        </a>
-         <span class="tooltip">Dashboard</span>
-      </li>
-      <li>
-       <a href="#">
-         <i class='bx bx-user' ></i>
-         <span class="links_name">User</span>
-       </a>
-       <span class="tooltip">User</span>
-     </li>
-  
-     <li>
-       <a href="statistics.php">
-         <i class='bx bx-pie-chart-alt-2' ></i>
-         <span class="links_name">Analytics</span>
-       </a>
-       <span class="tooltip">Analytics</span>
-     </li>
-    
-     
-   
-     <li>
-       <a href="#">
-         <i class='bx bx-cog' ></i>
-         <span class="links_name">Setting</span>
-       </a>
-       <span class="tooltip">Setting</span>
-     </li>
-     <li class="profile">
-         <div class="profile-details">
-           <img src="../assets/img/379-1662715378.jfif" alt="profileImg">
-           <div class="name_job">
-             <div class="name"><?php echo $user["username"]; ?></div>
-             
-           </div>
-         </div>
-         <a href="logout.php"><i class='bx bx-log-out' id="log_out" ></i></a>
-         
-     </li>
-    </ul>
-  </div>
+    <?php include '../includes/side.php';?>
+
+
   <section class="home-section">
       <div class="text">Dashboard</div>
       
@@ -91,36 +38,7 @@ else{
         
       </div>
 
-      <div class="history_lists">
-        <div class="list1">
-          <div class="row">
-            <h4>Articles</h4>
-            <!-- <a href="#">See all</a> -->
-          </div>
-          <div style="overflow-x:auto;">
-          <table>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>title</th>
-                <th>description</th>
-                <th>image</th>
-                <th>category</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>2, Aug, 2022</td>
-                <td>Sam Tonny</td>
-                <td>Premimum</td>
-                <td>AI</td>
-              </tr>
-              
-            </tbody>
-          </table>
-        </div>
-        </div>
+     
 
       
   </section>
---
+++ pages/posts.php
@@ -0,0 +1,101 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+<meta charset="UTF-8">
+<title> dashboard </title>
+<link rel="stylesheet" href="../assets/style.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>   
+</head>
+
+
+
+  <body>
+    
+<!-- sidebar -->
+    <?php include '../includes/side.php';?>
+
+
+
+        <section class="home-section">
+    <div class="text">Posts</div>
+      
+    <div class="main-body"> 
+        <div class="table-container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Image</th>
+                        <th scope="col">Category</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <td>@mdo</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        <td>@fat</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                        <td>@twitter</td>
+                        <td>@twitter</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                        <td>@twitter</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>         
+    </div>
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+
+  </section>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+  <script src="../assets/js/main.js"></script>
+            
+        </body>
